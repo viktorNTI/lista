4cbcf7626ed75a7aa9d947011dfe7f76c0caa925
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2018-11-28 12:44:57 +0100

    third commit

---
+++ lista.py
@@ -0,0 +1,12 @@
+import random
+
+val = 0
+
+while val !=5:
+    try:
+        int(input("(1) Skriv ut (2) Mata in (3) Rensa (4) Avsluta"))
+    except: 
+        print("Vänligen tryck (1), (2), (3) eller (4)")
+    if val == 1: 
+        
+
